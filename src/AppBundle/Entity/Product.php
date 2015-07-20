e4e8cf9f1cadf117a34abe7a627a058b0537d5b9
Licya <licya.puleio@gmail.com>
2015-07-20 20:24:04 +0200

    Creation of a php class called Product
    Creation of a php class called Product. This php class will be an entity
    in the resto database.

---
+++ src/AppBundle/Entity/Product.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Product
+ *
+ * @author Lilly
+ */
+
+namespace AppBundle\Entity;
+
+class Product {
+    protected $name;
+    protected $description;
+    protected $main_price;
+    protected $second_price;
+    protected $enable;
+}
